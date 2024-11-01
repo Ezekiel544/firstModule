31399b85bc0ef985098018d61569728d184e0709
Ezekiel Chikezie <chikeziezekiel3@gmail.com>
2024-11-01 10:00:03 +0100

    first commit

---
+++ classwork.js
@@ -0,0 +1,48 @@
+ export let myData = {
+    firstName : `Neche` ,
+    surName : `chikezie` ,
+    age :  25 ,
+    hub : `Rad5 Tech Hub ` ,
+    height : 5.9 ,
+    complexion : `dark`,
+    ID : 2.9  ,
+    married : false 
+}
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="style.css">
+//     <title>REGISTERTION_WEBPAGE</title>
+// </head>
+// <body>
+//     <form action="" method="post">
+//         <h3>School Registeration form</h3>
+//         <div>
+//            <label for="firstName">Enter your firstname</label>
+//            <input type="text" name="secondName" id="firstName"> 
+//        </div>
+//        <div>
+//            <label for="secondName">enter your secondname</label>
+//            <input type="text"  name="secondName" id="secondName">
+//        </div>
+//        <div>
+//            <label for="Email">enter your email</label>
+//            <input type="email" name="Email" id="userEmail">
+//        </div>
+//        <div>
+//            <label for="password">Enter your password</label>
+//            <input type="password" name="password" id="userPassword">
+//        </div>
+//        <div> 
+//            <label for="userConfirmation">i Agree to they terms and service of this project</label>
+//            <input type="checkbox" name="userConfirmation" id="userConfirmation" >
+//        </div>
+//            <button  id="submitBtn" type="submit"  >Register </button> <span>Already have an account ? <a href="#">login</a></span>
+   
+//    </form>
+
+//    <script src="./work.js"></script>
+// </body>
+// </html> 
---
+++ work.js
@@ -0,0 +1,6 @@
+
+import { myData } from "./classwork.js";
+console.log(`my name is ${myData.firstName} ${myData.surName} and i am ${myData.age} , i  do attend ${myData.hub} and i am ${myData.complexion} in complexion , with a height of ${myData.height} , and this is my ID ${myData.ID} , and my relationship status is ${myData.married}`);
+console.log(myData.surName);
+
+
